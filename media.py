4f9c947acb9752ca8d81c73924037dad6e0df86c
pedrokkjzk <167801838+Pedrohenrc@users.noreply.github.com>
2024-07-18 08:13:03 -0300

    Update media.py

--- media.py
+++ media.py
@@ -1,17 +1,14 @@
-#fazer um codigo que receba quantos numeros o usuario quiser e faça a media deles
 print ('Maquina de fabricar a media de quantos numeros quiser')
 X = float(input('Insira o primeiro numero:'))
 Y = float(input('Insira o segundo numero:'))
 num1 = X + Y
 cont = 2
 while True:
-    resposta = int(input('Deseja inserir outro numero? (Digite 1 para sim e 2 para não)'))
-    if resposta == 1:
+    resposta = str(input('Deseja inserir outro numero?')).lower()
+    if resposta == 'sim':
         A = float(input('Escolha o proximo numero:'))
         cont += 1
         num1 += A       
-    elif resposta == 2:
+    elif resposta == 'nao':
         print("a media dos numeros é", num1 / cont)
-        
-
         
